2dd134e8cbb662068f27997cba0a621de5c24836
a7465847 <qwe820610@gmail.com>
2026-01-21 07:18:51 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/salary-calculator/sw.js', { scope: '/salary-calculator/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
